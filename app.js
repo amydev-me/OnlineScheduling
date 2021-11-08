d7ba98a00f5646e018d87df757b6f788c0f32e13
Amy <ampyaephyonaing@gmail.com>
2021-11-08 19:47:26 +0800

    Port

--- app.js
+++ app.js
@@ -32,5 +32,5 @@ app.use(require("./routes/index"))
 require("./config/passport")
 
 app.listen(port, function () {
-    console.log('Server started on port ');
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
