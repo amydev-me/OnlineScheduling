ba3d896971ad3d1bca4c8ce9e442b450e5ee485b
Amy Pyae Phyo Naing <ampyaephyonaing@gmail.com>
2022-09-02 23:08:33 +0800

    Update database.js

--- Config/database.js
+++ Config/database.js
@@ -3,6 +3,6 @@ const mongoose=require('mongoose');
 /**
  * Connect To the database
  */
-mongoose.connect("mongodb+srv://amydev:Amy123!*@cluster0.ogfnn.mongodb.net/online_scheduling_tools?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://")
 .then(() => console.log("Database connected!"))
 .catch(err => console.log(err));
